7fe1ce80e1503481f044fc28003575e5211d03c9
supastishn <omareeto2012@hotmail.com>
2025-07-29 14:14:01 +0000

    feat: convert Node.js CLI to PyPI-compatible Python CLI tool

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="agentic",
+    version="0.0.1",
+    description="Open source, cheap, and premier coding agent",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "agentic=agentic.cli:main",
+        ],
+    },
+    author="",
+    license="ISC",
+)
