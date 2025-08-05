0f7a3c3fe65f252fae8374ba23dddfefa5c81c0c
supastishn <omareeto2012@hotmail.com>
2025-08-05 12:36:19 +0000

    build: bump version to 1.0.2 in setup.py for new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="agentic-cli-coder",
-    version="1.0.1",
+    version="1.0.2",
     description="Open source, cheap, and premier coding agent",
     long_description=long_description,
     long_description_content_type="text/markdown",
